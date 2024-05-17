3421663fcc43fef4e2c06e50b213c82a6c3adc72
leticialima222 <leticiaakiralele2018@gmail.com>
2024-05-17 18:32:54 +0000

    Criação dos primeiros passos

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { HomeContainer } from "./Home.style";
+
+export default function Home() {
+    return (
+        <HomeContainer>
+            <h1>Home</h1>
+        </HomeContainer>
+    )
+}
---
+++ src/pages/Home.style.js
@@ -0,0 +1,7 @@
+import style from "styled-components"
+
+export const HomeContainer = style.div`
+h1 {
+    color: blue;
+}
+`
